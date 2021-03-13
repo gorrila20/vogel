ea37c61d3495dfc3eaf93897ba2848f50e55619d
Martijn van Zuilen <mzuilen@ibleerling.nl>
2021-03-13 21:22:04 +0100

    Allows passing of arguments

--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 #include <SDL2/SDL.h> 
 #include <SDL2/SDL_image.h> 
 #include <SDL2/SDL_timer.h> 
+#include <unistd.h>
 #define DEFAULT_BIRD_SPEED_X 1
 #define DEFAULT_BIRD_SPEED_Y 1
 const unsigned int windowX = 1000; //Default values can be overriden
 const unsigned int windowY = 1000;
+
+int s_speedX = 1; //Standard bird x speed
+int s_speedY = 1; //Standard bird y speed
 
 struct vogel //Create data structure voor vogel
 {
@@ -20,7 +24,7 @@ struct vogel //Create data structure voor vogel
 
 struct vogel* vogels; //Declare dynamically allocated array
 
-int initialize_vogels_data(int n, int s_speedX, int s_speedY) //n=number of birds
+void initialize_vogels_data(int n, int s_speedX, int s_speedY) //n=number of birds
 {
     //TODO: Initialize datastructure for data
     vogels = (struct vogel * ) malloc((n+1) * sizeof(struct vogel));
@@ -47,15 +51,20 @@ int initialize_vogels_data(int n, int s_speedX, int s_speedY) //n=number of bird
         j++;
 
     }
-    return 0; 
-
 }
 
+void usage()
+{
+    fprintf(stderr, "-n <number of birds to draw> \n-x <bird default x speed> \n-y <bird default y speed> \n-h show this message\nEXAMPLE: vogel -n 30 -x 20 -y 10\n");
+    exit(EXIT_FAILURE);
+}
 int initialize_vogels_SDL()
 {
 
     //TODO: Initialize vogels to draw
     fprintf(stderr, "TODO!");
+
+    return 0;
 }
 
 int init_vogels(int n, int s_speedX, int s_speedY)
@@ -78,9 +87,40 @@ int d_calculateSpeedVector(int massOne, int massTwo, int v) //Usage voor vogel: 
 
 int main(int argc, char** argv) 
 { 
+    
+    int n = 20; //Default value
+   
+
+ 
+        int opt_index = 0; //Option index for passing arguments
+
+        while (( opt_index = getopt(argc, argv, "n:x:y:h")) != -1)
+        {
+                 switch(opt_index)
+                {
+                 case 'n':
+                    n = atoi(optarg);
+                    break;
+                 case 'h':
+                    usage();
+                    break;
+                 case 'x':
+                    s_speedX = atoi(optarg);
+                    break;
+                 case 'y':
+                   s_speedY = atoi(optarg);
+                   break;
+
+                }
+
+        }
+        
+        if (optind < argc)
+         usage();
+    
+    init_vogels(n, s_speedX, s_speedY);
+    
     //Main setup
-   const int n = 20000; //Start with 20 birds
-   init_vogels(n, DEFAULT_BIRD_SPEED_X, DEFAULT_BIRD_SPEED_Y); 
     for(int i =0; i<=n; i++)
     {
         printf("X: %d Y: %d SpeedX: %d SpeedY: %d\n", vogels[i].x, vogels[i].y, vogels[i].speedX, vogels[i].speedY);
@@ -122,7 +162,7 @@ int main(int argc, char** argv)
 
     }
          
-  
+    free(vogels);  
     return 0; 
 }
 
